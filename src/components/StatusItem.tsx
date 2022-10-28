1629a10f91da93f7251748925e8e591386a7404a
Didier Osindero <didierosindero@gmail.com>
2022-10-28 17:29:09 +0100

    Formatted src

--- src/components/StatusItem.tsx
+++ src/components/StatusItem.tsx
@@ -11,8 +11,8 @@ function StatusItem(props: StatusItemProps): JSX.Element {
   return (
     <li>
       {props.emoji} <a href="#">{props.username} </a>
-        <small>({formTimeString(props.timeAgo.n, props.timeAgo.unit)} ago)</small>: 
-      {props.content}
+      <small>({formTimeString(props.timeAgo.n, props.timeAgo.unit)} ago)</small>
+      :{props.content}
     </li>
   );
 }
